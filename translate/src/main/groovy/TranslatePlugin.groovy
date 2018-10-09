637bcd81404f67bf3316ea381314f347ee3d8b53
dingbaosheng <dbs@33.cn>
2018-10-09 18:22:52 +0800

    init

---
+++ translate/src/main/groovy/TranslatePlugin.groovy
@@ -0,0 +1,141 @@
+package com.fzm.translate
+
+import groovy.json.JsonSlurper
+import groovy.xml.MarkupBuilder
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class TranslatePlugin implements Plugin<Project> {
+
+    def mJsonSlurper = new JsonSlurper()
+
+    @Override
+    void apply(Project project) {
+
+
+        project.task('translate') {
+
+            doLast {
+
+                def valuesDir = new File(project.getProjectDir().getPath() + "/src/main/res/values")
+                def enValuesDir = new File(project.getProjectDir().getPath() + "/src/main/res/values-en")
+
+                valuesDir.traverse { file ->
+
+                    if (file.file && file.name.contains("strings")) {
+
+                        println("fileName:" + file.name)
+
+                        //读取汉字xml文件
+                        Map<String, String> map = parseStringXml(file)
+                        println("fileContent:" + map)
+                        println("===========================")
+
+                        //对汉字进行翻译处理
+                        Map<String, String> newMap = new HashMap<>()
+                        map.each {entry->
+
+                            String newValue = translate(entry.value.toString())
+                            newMap.put(entry.key.toString(), newValue.toString())
+                            println("key:"+entry.key+" value:"+entry.value)
+
+                        }
+
+                        //写到新文件中
+                        writeFile(new File(enValuesDir, "test_"+file.name), newMap)
+                    }
+                }
+            }
+        }
+    }
+
+    private String translate(String query) {
+
+
+        String response = com.github.kevinsawicki.http.HttpRequest.post("https://openapi.youdao.com/api",
+                http.TranslateHttp.buildRequest(query),
+                true).body()
+
+        //Json 解析
+        System.out.println("Response was: " + response)
+
+        def responseBean = mJsonSlurper.parseText(response)
+
+        String result = ''
+        if (responseBean
+                && responseBean.web
+                && responseBean.web.get(0).value) {
+            result = responseBean.web.get(0).value.get(0)
+        }
+
+        return result
+    }
+
+
+    private Map<String, String> parseStringXml(def file) {
+
+        Map<String, String> map = new HashMap<>()
+
+        def xmlString = new XmlSlurper().parse(file)
+        xmlString.string.each {
+            map.put("${it.@name}".toString(), "${it}".toString())
+        }
+
+        return map
+    }
+
+    private void writeFile(File saveFile, Map<String, String> map) {
+
+        /**
+         * 如果对应文件存在，则追加相关数据
+         * 否则创建文件即可
+         */
+        if (saveFile.exists()) {
+
+            Map<String, String> existMap = parseStringXml(saveFile)
+
+
+            def writer = new StringWriter()
+            def xml = new MarkupBuilder(writer)
+
+            xml.resources() {
+
+                existMap.each { entry ->
+
+                    string(name: "$entry.key", "$entry.value")
+                }
+
+
+                writer.write("\n\n  <!--ADD STRING START-->")
+                map.each { entry ->
+
+                    if (!existMap.containsKey("$entry.key".toString())) {
+                        string(name: "$entry.key", "$entry.value")
+                    }
+                }
+                writer.write("\n  <!--ADD STRING END-->\n")
+            }
+
+            FileWriter fw = new FileWriter(saveFile, false)
+            fw.write(writer.toString())
+            fw.close()
+
+        } else {
+            //create file
+
+            def writer = new StringWriter()
+            def xml = new MarkupBuilder(writer)
+
+            xml.resources() {
+
+                map.each { entry ->
+                    string(name: "$entry.key", "$entry.value")
+                }
+            }
+
+            FileWriter fw = new FileWriter(saveFile)
+            fw.write(writer.toString())
+            fw.close()
+        }
+    }
+}
---
+++ translate/src/main/java/http/TranslateHttp.java
@@ -0,0 +1,63 @@
+package http;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TranslateHttp {
+
+    public static Map<String, String> buildRequest(String query) {
+
+        String appKey = "08e46a6bb8402dd9";
+        String salt = String.valueOf(System.currentTimeMillis());
+        String from = "EN";
+        String to = "zh-CHS";
+        String sign = md5(appKey + query + salt + "70GVWg2u8KNjF7aRUvz1pw7gI8r5qPR1");
+        HashMap params = new HashMap();
+        params.put("q", query);
+        params.put("from", from);
+        params.put("to", to);
+        params.put("sign", sign);
+        params.put("salt", salt);
+        params.put("appKey", appKey);
+
+        return params;
+    }
+
+    /**
+     * 生成32位MD5摘要
+     *
+     * @param string
+     * @return
+     */
+    private static String md5(String string) {
+        if (string == null) {
+            return null;
+        }
+        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F'};
+
+        try {
+            byte[] btInput = string.getBytes("utf-8");
+            /** 获得MD5摘要算法的 MessageDigest 对象 */
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            /** 使用指定的字节更新摘要 */
+            mdInst.update(btInput);
+            /** 获得密文 */
+            byte[] md = mdInst.digest();
+            /** 把密文转换成十六进制的字符串形式 */
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (byte byte0 : md) {
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            return null;
+        }
+    }
+}
